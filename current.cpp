1d9e848cc199927a0b884aff57802d4bfecc3b9b
Kevin M <kevinmarx14@gmail.com>
2022-02-16 17:45:51 -0800

    current init and read files

---
+++ current.cpp
@@ -0,0 +1,24 @@
+#include "telemetry.h"
+#include "current.h"
+
+void current_init(){
+  // Set the current pin as an input
+  pinMode(CURRENT_PIN, INPUT);
+  return;
+}
+
+telem_point_t sample_current(){
+  // Take N_AVG readings from the TIA, average them, and convert to a current value in amps
+  telem_point_t data;
+  int reading = 0;
+
+  for(int i = 0; i < N_AVG; i++){
+    reading += analogRead(CURENT_PIN);
+  }
+  data.timestamp = millis();
+
+  reading /= N_AVG;
+  data.data.data_value = (reading - CURRENT_OFFSET)/CURRENT_GAIN;
+  
+  return(data);
+}
