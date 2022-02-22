18c50680825771b57bb8f64a7a74e17e5df73ef4
Kevin M <kevinmarx14@gmail.com>
2022-02-22 15:26:09 -0800

    Tried to fix single file decompression

--- sd_data_to_csv/sd_data_to_csv.ino
+++ sd_data_to_csv/sd_data_to_csv.ino
@@ -25,46 +25,39 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   Serial.println("Choose a file to decode to CSV:");
-  while(!Serial.available()) {};
-  //char filename[128];
-  String filename = Serial.readString();
+  while(!Serial.available()) {}; // wait for data to appear on serial bus
+  String filename = Serial.readString(); // Read the filename
 
   // open specified file
   File logfile = SD.open(filename, FILE_READ);
   if(logfile) {
     Serial.print("Decoding ");
     Serial.println(filename);
-    // add .csv for the decoded file
-    String new_filename = filename + ".csv";
-    File csvfile = SD.open(new_filename, FILE_WRITE);
 
-    char data_buf[8];
+    char data_buf[9];
     while(logfile.available()) {
-      logfile.read(data_buf, 8);
-      // first 4 bytes are 32 bit timestamp
-      uint32_t timestamp = data_buf[0] + (data_buf[1] << 8) + (data_buf[2] << 16) + (data_buf[3] << 24);
+      logfile.read(data_buf, 9);
+      // first 1 byte is identifier
+      char id = data_buf[0];
+      // next 4 bytes are 32 bit timestamp
+      uint32_t timestamp = data_buf[1] + (data_buf[2] << 8) + (data_buf[3] << 16) + (data_buf[4] << 24);
       // second 4 bytes are float data
       union {
         uint32_t data_bits;
         float data_val;
       } data;
-      data.data_bits = data_buf[4] + (data_buf[5] << 8) + (data_buf[6] << 16) + (data_buf[7] << 24);
+      data.data_bits = data_buf[5] + (data_buf[6] << 8) + (data_buf[7] << 16) + (data_buf[8] << 24);
 
-      // output decoded data point to both serial and csv
-      //Serial.print("timestamp: ");
-      //Serial.print(timestamp);
-      //Serial.print(" data: ");
-      //Serial.println(data.data_val);
-
+      // Open the corresponding CSV file
+      String new_filename = String(id) + ".csv";
+      File csvfile = SD.open(new_filename, FILE_WRITE);
       csvfile.print(timestamp);
       csvfile.print(",");
       csvfile.println(data.data_val, 10);
+      csvfile.close();
     }
 
-    csvfile.close();
     logfile.close();
   } else {
     Serial.print("Failed to open ");
