d6d38fe1e6cd076360c0aea11d185379c07fc592
Kevin M <kevinmarx14@gmail.com>
2022-02-23 17:04:45 -0800

    implemented thermocouple calibration

--- ee154a_payload_sw/thermistor.cpp
+++ ee154a_payload_sw/thermistor.cpp
@@ -1,6 +1,7 @@
 #include "telemetry.h"
 #include "thermistor.h"
 #include <Arduino.h>
+#include <math.h>
 
 telem_point_t sample_temp_internal(){
   // Take N_AVG readings from the thermistor, average them, and convert to a current value in amps
@@ -13,6 +14,9 @@ telem_point_t sample_temp_internal(){
   data.timestamp = millis();
 
   float read = reading/N_AVG;
+  float voltage = read * (3.3/1023.0);
+  float R = (2000 * voltage)/(1 - voltage/3.3);
+  float T = pow(a1 + b1 * log(R/Rref) + c1 * pow(log(R/Rref),2) + d1 * pow(log(R/Rref),3), -1) - 273.15 - 1.32;
   data.data.data_value = read;
     
   return data;
--- ee154a_payload_sw/thermistor.h
+++ ee154a_payload_sw/thermistor.h
@@ -1,6 +1,14 @@
 #include "telemetry.h"
+#include <math.h>
 
 #define TEMP_PIN       A2
 #define N_AVG          32
+
+const float Rref = 10000;
+const float a1 = 3.35 * pow(10,-3);
+const float b1 = 2.57 * pow(10,-4);
+const float c1 = 2.62 * pow(10,-6);
+const float d1 = 6.38 * pow(10,-8);
+
 
 telem_point_t sample_temp_internal();        // record internal temperature
